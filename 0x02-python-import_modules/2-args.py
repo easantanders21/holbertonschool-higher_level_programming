b56db0a7f55188f8a7b53b61217bce31e747218a
easantanders21 <3823@holbertonschool.com>
2021-12-28 01:22:34 +0000

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,13 @@ from sys import *
 def main():
     if len(argv) <= 1:
         print("0 arguments.")
-        return()
-    print("{} argument:".format(len(argv) - 1))
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+    elif len(argv) == 2:
+        print("{} argument:".format(len(argv) - 1))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
 
 
 if __name__ == '__main__':
