7d8d8751ac1a780ffe94882d1b14743cd77f06a5
easantanders21 <3823@holbertonschool.com>
2022-01-03 22:49:18 +0000

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if length == 0:
+    if length == None:
         new_tuple = (0, None)
     new_tuple = (length, sentence[0])
     return(new_tuple)
