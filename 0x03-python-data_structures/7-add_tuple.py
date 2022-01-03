4d3cead1ad1643d8f729404ffb4196977bc33dec
easantanders21 <3823@holbertonschool.com>
2022-01-03 22:24:54 +0000

    task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a_0 = 0
+    b_0 = 0
+    a_1 = 0
+    b_1 = 0
+    if len(tuple_a) >= 2:
+        a_0 = tuple_a[0]
+        a_1 = tuple_a[1]
+    elif len(tuple_a) == 1:
+        a_0 = tuple_a[0]
+
+    if len(tuple_b) >= 2:
+        b_0 = tuple_b[0]
+        b_1 = tuple_b[1]
+    elif len(tuple_b) == 1:
+        b_0 = tuple_b[0]
+    new_tuple = (a_0 + b_0, a_1 + b_1)
+    return(new_tuple)
