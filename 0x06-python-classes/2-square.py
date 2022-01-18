280b5f6e35abda83f7230dc55d03fc11d6c4e7a1
easantanders21 <3823@holbertonschool.com>
2022-01-18 22:39:02 +0000

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+" A class Square that defines a sixe for a square "
+
+
+class Square:
+    """
+    Square class
+    Attributes:
+        size (no type/value verification) : Private instance attribute
+    """
+    __size = None
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise NameError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
