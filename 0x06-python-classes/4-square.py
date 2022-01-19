a969165ecc65e453ed7f6332a0d937698546fc65
easantanders21 <3823@holbertonschool.com>
2022-01-19 02:49:05 +0000

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,7 +8,7 @@ class Square:
     """
     Square class
     Attributes:
-        size (no type/value verification) : Private instance attribute
+        size (int) : Private attribute
     """
     __size = None
 
@@ -19,9 +19,11 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if type(value) is not int:
