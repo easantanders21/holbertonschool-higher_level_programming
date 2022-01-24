fa85397dd668862b114fac491e03023b6cd6b0de
easantanders21 <3823@holbertonschool.com>
2022-01-24 03:33:03 +0000

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A module add integer - add two integers """
+
+
+def add_integer(a, b=98):
+    """ A function that add two integers """
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+_0x07. Python - Test-driven development_
