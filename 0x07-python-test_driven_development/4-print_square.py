bedd02d42a97a82ed02a8660dbbe415ebbe90339
easantanders21 <3823@holbertonschool.com>
2022-01-24 18:13:17 +0000

    task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write a function that prints a square with the character #."""
+
+
+def print_square(size):
+    """function that prints a square with the character #."""
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
