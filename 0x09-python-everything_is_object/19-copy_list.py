2962488a0a77bd9d00d332804d91da7d8cc76c70
easantanders21 <3823@holbertonschool.com>
2022-01-26 00:12:38 +0000

    task 19

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-"""A function that returns a copy of a list"""
-
-
-def copy_list(l):
-    """ copy a list """
-    new_list = l[:]
-    return new_list
+def copy_list(h):
+    return h[:]
