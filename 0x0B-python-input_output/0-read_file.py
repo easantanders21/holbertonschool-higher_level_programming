f4ff72d9f0f3c2eca50f4bf6d8a4bb6497f52f9f
easantanders21 <3823@holbertonschool.com>
2022-02-01 21:55:42 +0000

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,8 +2,8 @@
 """Function that reads a text file (UTF8) and prints it to stdout"""
 
 
-def read_file(filename="", mode="r", encoding="UTF8"):
+def read_file(filename=""):
     """Function that reads a text file (UTF8) and prints it to stdout """
-    with open(filename) as f:
+    with open(filename, mode="r", encoding="UTF8") as f:
         for line in f:
             print(line, end="")
