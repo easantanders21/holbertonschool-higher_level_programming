f3f3d35cfb6661521833495e9c0a8c0ab8e85d09
easantanders21 <3823@holbertonschool.com>
2022-03-16 18:53:38 -0500

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
--- Script that lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id, cities.name, states.name FROM cities, states ORDER BY id ASC;
+-- Script that lists all cities contained in the database hbtn_0d_usa
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.name = states.name ORDER BY cities.id ASC;
