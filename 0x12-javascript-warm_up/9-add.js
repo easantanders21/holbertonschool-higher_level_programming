fd935784e60afda85570bea2900d9b86f4fccab2
easantanders21 <3823@holbertonschool.com>
2022-04-04 16:57:56 -0500

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
 if (isNaN(args[2]) || isNaN(args[3])) {
-  console.log('isNaN');
+  console.log('NaN');
 } else {
   let sum = 0;
   sum = add(parseInt(args[2]), parseInt(args[3]));
