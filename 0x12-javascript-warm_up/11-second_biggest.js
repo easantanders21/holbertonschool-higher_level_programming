e69b741749dfd02ad49b09e246afebf34f06183b
easantanders21 <3823@holbertonschool.com>
2022-04-04 18:01:38 -0500

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const args = process.argv;
-const array_sort = [];
+let i = 2;
+const array = [];
 if (args.length <= 3) {
   console.log(0);
 } else {
-  let i = 2;
   while (args[i]) {
-    array_sort.push(parseInt(args[i]));
+    array.push(parseInt(args[i]));
     i++;
   }
-  array_sort.sort(compareNumbers);
-  console.log(array_sort[array_sort.length - 2]);
+  array.sort(compareNumbers);
+  console.log(array[array.length - 2]);
 }
 
 function compareNumbers (a, b) {
