01b4374b76b4669af8eec7a71fcc08e5e618700a
easantanders21 <3823@holbertonschool.com>
2022-04-12 10:48:27 -0500

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,8 +2,6 @@
 const request = require('request');
 const args = process.argv;
 const url = args[2];
-let flag = 0;
-let count = 1;
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
@@ -12,12 +10,7 @@ request(url, function (error, response, body) {
     const dictionary = {};
     for (const keys in obj) {
       if (obj[keys].completed === true) {
-        count++;
-        dictionary[obj[keys].userId] = count;
-        if (obj[keys].userId !== flag) {
-          count = 1;
-        }
-        flag = obj[keys].userId;
+        dictionary[obj[keys].userId] = dictionary[obj[keys].userId] + 1 || 1;
       }
     }
     console.log(dictionary);
