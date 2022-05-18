b0affb8c9a04d86c2db79676cba437a1c60f5252
easantanders21 <3823@holbertonschool.com>
2022-05-18 21:52:45 +0000

    task 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL, and displays the body of the response
-curl -s -X GET "$1" -H "X-HolbertonSchool-User-Id: 98"
+curl -s -X GET "$1" -H "X-School-User-Id: 98"
