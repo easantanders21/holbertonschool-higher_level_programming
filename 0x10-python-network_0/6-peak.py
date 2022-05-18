83f40b39000610c816ca82c568b24009c3e1943e
easantanders21 <3823@holbertonschool.com>
2022-05-18 18:10:50 -0500

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Function find_peak """
+
+
+def find_peak(list_of_integers):
+    """ find peak """
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
+    return None
