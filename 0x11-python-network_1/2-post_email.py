ae52acea642fcd848896ecf81747bbd2ac8b3737
easantanders21 <3823@holbertonschool.com>
2022-05-19 18:44:49 -0500

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Write a Python script that fetches """
+import urllib.request
+from sys import argv
+
+
+if __name__ == "__main__":
+    """ main """
+    data = urllib.parse.urlencode({'email': argv[2]})
+    data = data.encode('ascii')
+    with urllib.request.urlopen(argv[1], data) as response:
+        html = response.read()
+    html = html.decode('ascii')
+    print(html)
