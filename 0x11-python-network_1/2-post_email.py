2f0d6040fc44c48c971413fbf7e5ee3a37774150
easantanders21 <3823@holbertonschool.com>
2022-05-19 18:47:35 -0500

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,7 +5,6 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    """ main """
     data = urllib.parse.urlencode({'email': argv[2]})
     data = data.encode('utf-8')
     with urllib.request.urlopen(argv[1], data) as response:
