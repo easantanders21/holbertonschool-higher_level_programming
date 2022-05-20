7b4387884e50cadaffdf4534c3c2f1532007f858
easantanders21 <3823@holbertonschool.com>
2022-05-19 21:37:03 -0500

    task8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,18 +3,19 @@
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5000/search_user"
+    url = 'http://0.0.0.0:5000/search_user'
+    values = {'q': ""}
     if len(argv) == 2:
-        values = {'q': argv[1]}
+        values['q'] = argv[1]
+        res = requests.post(url, data=values)
     else:
-        values = {'q': ""}
-    res = requests.post(url, values)
+        res = requests.post(url, data=values)
     try:
-        if (len(res.json()) > 0):
-            print('[{}] {}'.format(res.json()['id'], res.json()['name']))
-        else:
+        user = response.json()
+        if len(user) < 1:
             print("No result")
-    except Exception:
+        else:
+            print("[{}] {}".format(user["id"], user["name"]))
+    except ValueError:
         print("Not a valid JSON")
