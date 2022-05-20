13e458148776d4d5454a0e43dd7e3928d87372fb
easantanders21 <3823@holbertonschool.com>
2022-05-19 19:40:42 -0500

    task5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Script that uses a REST API """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """ main """
+    args = requests.get(argv[1]).headers['X-Request-Id']
+    print(args)
